4d75b2987fd37bbeebd4b101bc8a0fbc5d84b176
vamshiInqui-lab <vamshi@inqui-lab.org>
2022-03-21 22:15:06 +0530

    move swagger service to createservice fuction

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,19 +1,25 @@
-import Express from 'express';
+import Express, { Request, Response } from 'express';
 import cors from 'cors'
 import deserializerUser from '../middleware/student/deserializerUser';
 import routes from '../routes';
+import swaggerDocs from './swagger';
+
 
 /**
  * create the express server when it's called.
  * @returns App
  */
-function createServer() {
+function createServer(Port: number) {
     const App = Express();
     App.use(Express.json());
     App.use(Express.urlencoded({ extended: true }));
     App.use(cors())
     App.use(deserializerUser);
     routes(App);
+    swaggerDocs(App, Port);
+    App.use("*", (req: Request, res: Response) => {
+        res.status(404).send({ message: "Page not found" })
+    })
     return App;
 }
 
