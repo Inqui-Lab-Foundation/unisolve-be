4c265c20cb691eff76883473b2006985b1f71b0c
vamshiInqui-lab <vamshi@inqui-lab.org>
2022-03-21 22:14:07 +0530

    database configration changed to prod db

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('ASE', 'root', '12345', {
-    host: 'localhost',
+const db = new Sequelize('dbunisolve', 'unisolvedbmaster', 'f1b(!3c8#pu2Jy45eFEo5O.}YcMO6?y2', {
+    host: 'ls-26f6efb9ac86d2cc1ee72fb9e4d19e9b172da76d.chqqmkdqkqez.ap-south-1.rds.amazonaws.com',
     dialect: "mysql",
     logging: false,
 });
