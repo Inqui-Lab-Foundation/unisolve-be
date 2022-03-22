f4e1dc8beff046d71ebf44be3af7dbd65c4d4df7
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-22 15:21:24 +0530

    install npm

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install v14.15.3
 npm install pm2 -g && pm2 update
 pm2 stop all
 pm2 delete all
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npm install pm2 -g && pm2 update
+# npm install pm2 -g && pm2 update
+echo 'Installation completed'
