8db4c4f131613e8b1281ef47d230d85dda0cc7a3
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-22 14:49:12 +0530

    install pm2

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -2,5 +2,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+npm install pm2 -g && pm2 update
 pm2 stop all
 pm2 delete all
