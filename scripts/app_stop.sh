37a7db550bdef8e5c82a60f72ffa30de83feac9a
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-22 16:38:15 +0530

    Fixed npm issues

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -3,7 +3,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install v14.15.3
-npm install pm2 -g && pm2 update
+nvm install node
+npm install pm2@latest -g
 pm2 stop all
-pm2 delete all
