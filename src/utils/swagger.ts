ba2816271f6bfb6dd24034bc4cd976f20e8a1dc4
vamshiInqui-lab <vamshi@inqui-lab.org>
2022-03-22 16:07:10 +0530

    added new configuration

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,52 +1,7 @@
 import { Express, Request, Response } from 'express';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { version } from '../../package.json';
 import logger from './logger';
-
-// define Swagger options with specific properties
-const options: swaggerJSDoc.Options = {
-    definition: {
-        openapi: "3.0.1",
-        info: {
-            title: "Unislove API Docs",
-            description: "Unislove backend applications api documentation with in details API description",
-            version,
-            license: {
-                name: 'Apache 2.0',
-                url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:3002',
-                description: 'Local Server',
-            },
-            {
-                url: 'http://15.207.254.154:3002',
-                description: 'Production Server',
-            },
-        ],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: "http",
-                    scheme: "bearer",
-                    bearerFormat: "JWT",
-                },
-            },
-        },
-        security: [
-            {
-                bearerAuth: [],
-            },
-        ],
-    },
-    apis: ["./src/routes.ts", "./src/schemas/*.ts"],
-};
-
-// passing swagger option to swaggerJSdoc built-in function
-const swaggerSpec = swaggerJSDoc(options);
+import { options } from '../docs/options'
 
 /**
  * Swagger Ui application starter
@@ -54,10 +9,10 @@ const swaggerSpec = swaggerJSDoc(options);
  * @param port septate port number other express app
  */
 function swaggerDocs(app: Express, port: number) {
-    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(options));
     app.get('/docs.json', (req: Request, res: Response) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec);
+        res.send(options);
     });
     logger.info(`Docs available at http://localhost:${port}/docs`);
 };
