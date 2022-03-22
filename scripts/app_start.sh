337f44179fbdf66d9390c88e310c9afea4d78014
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-22 21:12:29 +0530

    runas ubuntu

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,6 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
+npm install -g pm2@latest
 sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
 cd /home/ubuntu/
 pm2 start ./dist/src/index.js
