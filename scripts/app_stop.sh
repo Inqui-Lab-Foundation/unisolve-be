c1d66c3a1e40ba88ccdf5bd35d5cf940d1a87f0f
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-22 16:52:10 +0530

    Pm2 home

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
-npm install pm2@latest -g
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+cd /home/ubuntu/
 pm2 stop all
