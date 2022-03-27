96d57331e5c8ffa3177ae6fdff744e39a219827c
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-27 23:12:55 +0530

    Npm start prod (#30)

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -3,6 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
-sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+export PM2_HOME=/home/ubuntu/.pm2
+pm2 list
 cd /home/ubuntu/
 pm2 stop all
+pm2 delete all
