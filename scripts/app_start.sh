493fa86fedb47dcde00b2f58534382c488c1c7ea
Pradeep Gandla <pradeepdoit@gmail.com>
2022-03-27 23:12:26 +0530

    Npm start prod

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,8 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
-npm install -g pm2@latest
 export PM2_HOME=/home/ubuntu/.pm2
 pm2 list
 cd /home/ubuntu/
-pm2 start ./unisolve-be/src/index.js
+npm run start:prod
+pm2 save --force
