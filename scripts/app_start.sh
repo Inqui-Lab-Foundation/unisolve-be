59dce8c3da944c6e3c099aa390b40655c66222fc
Pradeep Gandla <pradeepdoit@gmail.com>
2022-04-04 07:01:18 +0530

    npm install and build (#33)

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -4,7 +4,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
 export PM2_HOME=/home/ubuntu/.pm2
-pm2 list
-cd /home/ubuntu/
+pm2 stop all
+pm2 delete all
+cd /home/ubuntu/unisolve-be
+npm install
+npm run build
 npm run start:prod
 pm2 save --force
