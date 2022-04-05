987b7d78b9326b32de4d8edc6f5d0d062937d7a0
vamshiInqui-lab <vamshi@inqui-lab.org>
2022-04-05 22:09:48 +0530

    requested by frontend

--- src/models/mentor.model.ts
+++ src/models/mentor.model.ts
@@ -24,7 +24,7 @@ mentor.init(
         },
         mobile: {   
             type: DataTypes.BIGINT,
-            allowNull: false,
+            allowNull: true,
             unique: true
         },
         email: {
