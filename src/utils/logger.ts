24d13bcaf9fb4ad3bb674b055f3238abd05842d9
vamshiInqui-lab <vamshi@inqui-lab.org>
2022-04-06 20:21:14 +0530

    added new logger package

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,25 @@
-import logger from 'pino';
-import dayjs from 'dayjs';
+const path = require('path');
+const { createLogger, transports, format } = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
 
-/**
- * logger service from pino package
- * @param no parameters.
- * @return log message with timestamp.
- */
-const log = logger({
-    prettyPrint: true,
-    base: {
-        pid: false
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`
+// if (process.env.NODE_ENV !== 'test') {
+const logger = createLogger({
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+        format.printf(
+            (info: any) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+    ),
+    transports: [
+        new DailyRotateFile({
+            filename: path.join(__dirname, '..', 'logs/all-logs-%DATE%.log'),
+            json: false,
+            maxSize: 5242880,
+            maxFiles: 5,
+        }),
+        new transports.Console(),
+    ],
 });
+// }
 
-export default log;
+export default logger;
