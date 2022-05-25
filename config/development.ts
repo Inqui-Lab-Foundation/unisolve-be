90587cfedd3c17a77e79beda06dcff2014ce5295
amn <satija.aman@gmail.com>
2022-05-25 17:33:56 +0530

    added a development db , which is hosted at hostinger and can be accessed remotely

--- config/development.ts
+++ config/development.ts
@@ -1,8 +1,8 @@
 const devDb = {
-    databaseName: 'ASE',
-    userName: 'root',
-    password: 'PASSWORD',
-    hostName: 'localhost'
+    databaseName: 'u739793688_unisolve_db_v1',
+    userName: 'u739793688_inql_user_1',
+    password: 'inquiLAB@123',
+    hostName: '185.232.14.11'
 }
 
 export default devDb;
