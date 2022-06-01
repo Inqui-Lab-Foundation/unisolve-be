f11b45ae8f89fb2438098497f5d2c484dfa7770f
Harishkumar Reddy <harishkumarreddy.cherla@gmail.com>
2022-06-01 13:54:46 +0530

    removed all console logs

--- config/database.config.ts
+++ config/database.config.ts
@@ -5,7 +5,6 @@ import devDb from './development';
 let credentials: any;
 
 if (process.env.NODE_ENV === 'development') {
-    console.log(process)
     credentials = devDb;
     console.log('using the local Database');
 }
