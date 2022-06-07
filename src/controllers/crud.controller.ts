898feacb218a9cbdf3b9777839aad2d53adca71a
amn <satija.aman@gmail.com>
2022-06-07 07:57:16 +0530

    threw error from boom error_build now handles custom created httpException error readied the crud controller to be extended

--- src/controllers/crud.controller.ts
+++ src/controllers/crud.controller.ts
@@ -2,18 +2,24 @@ import { Router, Request, Response, NextFunction } from 'express';
 import IController from '../interfaces/controller.interface';
 import HttpException from '../utils/exceptions/http.exception';
 import CRUDService from '../services/crud.service';
+import { notFound } from 'boom';
 
 export default class CRUDController implements IController {
-    public path: string;
+    public path: string="";
     public router: Router;
     crudService: CRUDService = new CRUDService;
 
     constructor() {
-        this.path = '/crud';
+        this.initializePath();
         this.router = Router();
         this.initializeRoutes();
     }
-    private initializeRoutes(): void {
+
+    protected initializePath() {
+        this.path = '/crud';
+    }
+
+    protected initializeRoutes(): void {
         this.router.get(`${this.path}/:model`, this.getData);
         this.router.get(`${this.path}/:model/:id`, this.getData);
         this.router.post(`${this.path}/:model`, this.createData);
@@ -21,12 +27,12 @@ export default class CRUDController implements IController {
         this.router.delete(`${this.path}/:model/:id`, this.deleteData);
     }
 
-    private loadModel = async (model: string): Promise<Response | void | any> => {
+    protected loadModel = async (model: string): Promise<Response | void | any> => {
         const modelClass = await import(`../models/${model}.model`);
         return modelClass[model];
     }
 
-    private getData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    protected getData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             let data: any;
             const { model, id } = req.params;
@@ -39,7 +45,7 @@ export default class CRUDController implements IController {
                 }
 
                 if (!data) {
-                    throw new HttpException(404, 'Data not found');
+                    throw notFound('Data not found');
                 }
                 return res.status(200).send(data);
             });
@@ -48,12 +54,12 @@ export default class CRUDController implements IController {
         }
     }
 
-    private createData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    protected createData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { model } = req.params;
             const data = await this.crudService.create(await this.loadModel(model), req.body);
             if (!data) {
-                throw new HttpException(404, 'Data not found');
+                throw notFound('Data not found');
             }
             return res.status(200).send(data);
         } catch (error) {
@@ -61,12 +67,12 @@ export default class CRUDController implements IController {
         }
     }
 
-    private updateData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    protected updateData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { model, id } = req.params;
             const data = await this.crudService.update(await this.loadModel(model), req.body, { where: { id } });
             if (!data) {
-                throw new HttpException(404, 'Data not found');
+                throw notFound('Data not found');
             }
             return res.status(200).send(data);
         } catch (error) {
@@ -74,7 +80,7 @@ export default class CRUDController implements IController {
         }
     }
 
-    private deleteData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    protected deleteData = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { model, id } = req.params;
             const data = await this.crudService.delete(await this.loadModel(model), { where: { id } });
@@ -87,3 +93,5 @@ export default class CRUDController implements IController {
         }
     }
 }
+
+
