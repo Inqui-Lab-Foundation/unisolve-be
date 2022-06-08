919941145715a52cf5ebb014c7b29d34335a41e0
amn <satija.aman@gmail.com>
2022-06-08 21:08:35 +0530

    Validations

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -1,9 +1,14 @@
+import ValidationsHolder from "../validations/validationHolder";
+import { videoSchema, videoUpdateSchema } from "../validations/video.validations";
 import BaseController from "./base.controller";
 
 export default class VideoController extends BaseController {
 
     model = "video";
 
+    protected initializeValidations(): void {
+        this.validations =  new ValidationsHolder(videoSchema,videoUpdateSchema);
+    }
     protected initializePath(): void {
         this.path = '/videos';
     }
