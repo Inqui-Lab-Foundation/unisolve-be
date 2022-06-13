8391665dd33a8b5a683a0a3831f691a227ab9b22
amn <satija.aman@gmail.com>
2022-06-13 20:21:49 +0530

    Migrations: changed user and course modles to match the actual models itself..

--- src/migrations/migrations/2022.06.13T14.15.54.create_users.ts
+++ src/migrations/migrations/2022.06.13T14.15.54.create_users.ts
@@ -1,93 +1,90 @@
 import { Migration } from '../umzug';
 import { DataTypes } from 'sequelize';
-
+import { constents } from '../../configs/constents.config';
 // you can put some team-specific imports/code here to be included in every migration
 
 export const up: Migration = async ({ context: sequelize }) => {
 	await sequelize.getQueryInterface().createTable('users', {
 		user_id: {
-		  allowNull: false,
-		  autoIncrement: true,
-		  primaryKey: true,
-		  type: DataTypes.INTEGER
+			allowNull: false,
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
 		},
 		email: {
-		  allowNull: false,
-		  unique: true,
-		  type: DataTypes.STRING(55),
-		  
+			type: DataTypes.STRING(55),
+			allowNull: false,
+			unique: true
 		},
 		password: {
-		  allowNull: false,
-		  type: DataTypes.STRING
+			type: DataTypes.STRING,
+			allowNull: false,
+			// select: false
 		},
 		full_name: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING,
+			allowNull: true,
 		},
 		image: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING,
+			allowNull: true,
 		},
 		date_of_birth: {
-		  type: DataTypes.DATE
+			type: DataTypes.DATE,
+			allowNull: true
 		},
 		mobile: {
-		  unique: true,
-		  allowNull: false,
-		  type: DataTypes.STRING(50)
+			type: DataTypes.STRING(50),
+			unique: true,
+			allowNull: false
 		},
 		team_id: {
-		  allowNull:true,
-		  type: DataTypes.INTEGER
+			type: DataTypes.STRING,
 		},
 		org_name: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING,
 		},
 		qualification: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING,
+			allowNull: false
 		},
 		stream: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING
 		},
 		city: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING
 		},
 		district: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING
 		},
 		state: {
-		  type: DataTypes.STRING
+			type: DataTypes.STRING
 		},
 		country: {
-		  type: DataTypes.STRING
-		},
-		role: {
-		  type: DataTypes.ENUM('ADMIN', 'STUDENT', 'MENTOR','EVALUATER'),
-		  defaultValue: 'ADMIN'
-		},
-		is_loggedin: {
-		  type: DataTypes.ENUM('YES', 'NO'),
-		  defaultValue: 'NO'
-		},
-		last_login: {
-		  type: DataTypes.DATE
+			type: DataTypes.STRING
 		},
 		status: {
-		  type: DataTypes.ENUM('ACTIVE', 'INACTIVE', 'LOCKED','DELETED'),
-		  defaultValue: 'ACTIVE'
+			type: DataTypes.ENUM(...Object.values(constents.common_status_flags.list)),
+			defaultValue: constents.common_status_flags.default
+		},
+		role: {
+			type: DataTypes.ENUM(...Object.values(constents.user_role_flags.list)),
+			defaultValue: constents.user_role_flags.default
+		},
+		is_loggedin: {
+			type: DataTypes.ENUM(...Object.values(constents.common_yes_no_flags.list)),
+			defaultValue: constents.common_yes_no_flags.default
+		},
+		last_login: {
+			type: DataTypes.DATE
 		},
 		created_by: {
-		  type: DataTypes.INTEGER
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 1
 		},
 		updated_by: {
-		  type: DataTypes.INTEGER
-		},
-		created_at: {
-		  allowNull: false,
-		  type: DataTypes.DATE
-		},
-		updated_at: {
-		  allowNull: false,
-		  type: DataTypes.DATE
+			type: DataTypes.INTEGER
 		}
 	  });
 };
--- src/migrations/migrations/2022.06.13T14.27.59.create_courses.ts
+++ src/migrations/migrations/2022.06.13T14.27.59.create_courses.ts
@@ -1,37 +1,30 @@
 import { Migration } from '../umzug';
 import { DataTypes } from 'sequelize';
-
+import { constents } from '../../configs/constents.config';
 // you can put some team-specific imports/code here to be included in every migration
 
 export const up: Migration = async ({ context: sequelize }) => {
 	await sequelize.getQueryInterface().createTable('courses', {
 		course_id: {
-		  allowNull: false,
-		  autoIncrement: true,
-		  primaryKey: true,
-		  type: DataTypes.INTEGER
-		},
-		title: {
-		  allowNull: false,
-		  type: DataTypes.STRING
-		},
-		description: {
-		  type: DataTypes.STRING
-		},
-		status: {
-		  type: DataTypes.ENUM('ACTIVE', 'INACTIVE', 'DELETD')
-		},
-		thumbnail: {
-		  type: DataTypes.STRING
-		},
-		created_at: {
-		  allowNull: false,
-		  type: DataTypes.DATE
-		},
-		updated_at: {
-		  allowNull: false,
-		  type: DataTypes.DATE
-		}
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        thumbnail: {
+            type: DataTypes.STRING
+        },
+        status: {
+            type: DataTypes.ENUM(...Object.values(constents.common_status_flags.list)),
+            defaultValue: constents.common_status_flags.default
+        }
 	  });
 };
 
