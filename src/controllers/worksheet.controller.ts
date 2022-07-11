e25c011a26818b330e029e8179faaf3493346844
aman satija <satija.aman@gmail.com>
2022-07-12 01:31:14 +0530

    fixed worksheet submit api

--- src/controllers/worksheet.controller.ts
+++ src/controllers/worksheet.controller.ts
@@ -3,7 +3,7 @@ import { worksheetSchema, worksheetUpdateSchema } from "../validations/worksheet
 import ValidationsHolder from "../validations/validationHolder";
 import BaseController from "./base.controller";
 import { NextFunction, Request, Response } from "express";
-import { badRequest } from "boom";
+import { badRequest, internal, unauthorized } from "boom";
 import { speeches } from "../configs/speeches.config";
 import path from "path";
 import fs from 'fs';
@@ -30,6 +30,10 @@ export default class WorksheetController extends BaseController {
         if(!worksheet_id){
             throw badRequest(speeches.WORKSHEET_ID_REQUIRED);
         }
+        const user_id =  res.locals.user_id;
+        if(!user_id){
+            throw unauthorized(speeches.UNAUTHORIZED_ACCESS);
+        }
 
         const rawfiles: any = req.files;
         const files: any = Object.values(rawfiles);
@@ -44,12 +48,17 @@ export default class WorksheetController extends BaseController {
             let filename = file.path.split(path.sep).pop();
             filename = ""+Date.now()+"_"+filename
             const targetPath = path.join(process.cwd(), 'resources', 'static', 'uploads', 'worksheets' ,'responses', filename);
-            await fs.rename(file.path, targetPath, async (err) => {
+            await fs.copyFile(file.path, targetPath, async (err) => {
                 if (err) {
                     errs.push(`Error uploading file: ${file.originalFilename}`);
+                    // console.log(err)
+                    // throw internal(`Error uploading file: ${file.originalFilename}`)
+
                 } else {
-                    reqData[file.fieldName] = `/worksheets/responses/${filename}`;
-                    attachments  = attachments+`/worksheets/responses/${filename},`
+                    
+                    reqData[file.fieldName] = `/assets/worksheets/responses/${filename}`;
+                    attachments  = attachments+`/assets/worksheets/responses/${filename},`
+                    console.log(attachments)
                     //todo: add this fieldname to an comma separarted variable called atttachments
                 }
             });
@@ -63,8 +72,10 @@ export default class WorksheetController extends BaseController {
         
         let dataToBeUploaded:any = {};
         dataToBeUploaded["worksheet_id"]= worksheet_id
+        dataToBeUploaded["user_id"] = user_id
         dataToBeUploaded["attachments"]= attachments
         const payload = this.autoFillTrackingCollumns(req, res, modelLoaded, dataToBeUploaded)
         const data = await this.crudService.create(modelLoaded, payload);
+        res.status(200).send(dispatcher(data,"success"));
     }
 } 
