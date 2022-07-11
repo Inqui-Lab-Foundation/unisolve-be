06844ea3f29eaf0fde97dcb002c61ce89e76de78
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-07-11 14:56:00 +0530

    Added documentation

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -71,5 +71,5 @@ export const genericErrorHandler: ErrorRequestHandler = async (err: any, req: Re
   await logIt(constents.log_levels.list.ERROR, `${err.message}: ${err}`, req, res);
   const error = buildError(err);
   
-  res.status(error.code).json(dispatcher({}, 'error',error.message,error.code))
+  res.status(error.code).send(dispatcher({}, 'error',error.message,error.code))
 }
