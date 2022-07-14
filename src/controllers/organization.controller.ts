bdb49e44c747e9d3c5ba7832fb765ae277b24d73
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-07-14 22:17:46 +0530

    Update organization.controller.ts
    Functionality updated, and added no of record inserted counter to the response.

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,8 +1,8 @@
 import { badRequest, internal } from "boom";
-import csvParser from "csv-parser";
+import * as csv from "fast-csv";
 import { NextFunction, Request, Response } from "express";
 import fs from 'fs';
-import { date } from "joi";
+import { any, date } from "joi";
 import path from 'path';
 import { speeches } from "../configs/speeches.config";
 import dispatcher from "../utils/dispatch.util";
@@ -26,32 +26,54 @@ export default class OrganizationController extends BaseController {
         super.initializeRoutes();
     };
     protected async createDataWithFile(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        const Error: any = [];
         //@ts-ignore
-        if (req.files.data === undefined) {
-            console.log('first')
-            throw badRequest();
+        let file = req.files.data;
+        let Errors: any = [];
+        let organizations: any = [];
+        let counter: number = 0;
+        let existedEntities: number = 0;
+        let dataLength: number;
+        if (file === undefined) {
+            return res.status(400).send(dispatcher(null, 'error', 'file is required', 400));
         }
-        //@ts-ignore
-        const stream = fs.createReadStream(req.files.data.path).pipe(csvParser());
+        if (file.type !== 'text/csv') {
+            return res.status(400).send(dispatcher(null, 'error', 'CSV file is required', 400));
+        }
+        const modelLoaded = await this.loadModel(this.model);
+        const stream = fs.createReadStream(file.path).pipe(csv.parse({ headers: true }));
+        stream.on("error", (error) => {
+            return res.status(400).send(dispatcher(error, 'error', 'error while reading the file date', 400));
+        })
         stream.on('data', async (data: any) => {
-            if (Object.entries(data).length > 0) {
-                const modelLoaded = await this.loadModel(this.model);
-                const payload = this.autoFillTrackingCollumns(req, res, modelLoaded, data);
-                const preLoadedData: any = await this.crudService.findOne(modelLoaded, { where: { organization_name: data.organization_name } });
-                if (preLoadedData) {
-                    Error.push(badRequest().message);
-                } else {
-                    await this.crudService.create(modelLoaded, payload);
+            dataLength = Object.entries(data).length;
+            if (dataLength > 0) {
+                if (data.organization_name === '' || data.organization_code === '') {
+                    Errors.push(badRequest('missing fields', data));
+                    return;
                 }
+                organizations.push(data);
             }
         })
-        stream.on('error', error => Error.push(error));
-        stream.on('end', () => {
-            if (Error.length > 0) {
-                next(badRequest());
-            } else {
-                res.send(dispatcher(null, 'success', 'created', 200));
+        stream.on('end', async () => {
+            if (Errors.length > 0) next(badRequest(Errors.message));
+            for (let data = 0; data < organizations.length; data++) {
+                const payload = this.autoFillTrackingCollumns(req, res, modelLoaded, organizations[data]);
+                const match = await this.crudService.findOne(modelLoaded, { where: { organization_name: organizations[data].organization_name } });
+                if (match) {
+                    existedEntities++;
+                } else {
+                    counter++;
+                }
+            }
+            if (counter > 0) {
+                await this.crudService.bulkCreate(modelLoaded, organizations)
+                    .then((result) => {
+                        return res.send(dispatcher({ data: result, createdEntities: counter, existedEntities }, 'success', 'created', 200));
+                    }).catch((error: any) => {
+                        return res.status(500).send(dispatcher(error, 'error', 'something went wrong', 500));
+                    })
+            } else if (existedEntities > 0) {
+                return res.status(400).send(dispatcher({ createdEntities: counter, existedEntities }, 'error', 'error existing', 400));
             }
         });
     }
