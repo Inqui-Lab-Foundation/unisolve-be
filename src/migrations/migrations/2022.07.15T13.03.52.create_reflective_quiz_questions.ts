cf0d007865456b7d4ccaf96b6742d176e8f953f5
amn <satija.aman@gmail.com>
2022-07-15 20:19:29 +0530

    Reflective questions: first draft video apis now return reflective quetions

---
+++ src/migrations/migrations/2022.07.15T13.03.52.create_reflective_quiz_questions.ts
@@ -0,0 +1,110 @@
+import { Migration } from '../umzug';
+import { DataTypes } from 'sequelize';
+import { constents } from '../../configs/constents.config';
+
+// you can put some table-specific imports/code here
+export const tableName = "reflective_quiz_questions";
+export const up: Migration = async ({ context: sequelize }) => {
+	// await sequelize.query(`raise fail('up migration not implemented')`); //call direct sql 
+	//or below implementation 
+	await sequelize.getQueryInterface().createTable(tableName, {
+		reflective_quiz_question_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        video_id: {
+            type: DataTypes.INTEGER,
+            allowNull:false
+        },
+        question_no: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        question: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        option_a: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        option_b: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        option_c: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        option_d: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        correct_ans: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        level: {
+            type: DataTypes.ENUM(...Object.values(constents.quiz_question_level_flags.list)),
+            allowNull: false,
+            defaultValue: constents.quiz_question_level_flags.default
+        },
+        type: {
+            type: DataTypes.ENUM(...Object.values(constents.quiz_question_type_flags.list)),
+            allowNull: false,
+            defaultValue: constents.quiz_question_type_flags.default
+        },
+        redirect_to: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        question_image: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+
+        msg_ans_correct: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            defaultValue:"",
+        },
+        msg_ans_wrong: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            defaultValue:"",
+        },
+        status: {
+            type: DataTypes.ENUM(...Object.values(constents.common_status_flags.list)),
+            allowNull: false,
+            defaultValue: constents.common_status_flags.default
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue:null
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+            onUpdate: new Date().toLocaleString()
+        }
+	  });
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+	// 	await sequelize.query(`raise fail('down migration not implemented')`); //call direct sql 
+	//or below implementation 
+	await sequelize.getQueryInterface().dropTable(tableName);
+};
