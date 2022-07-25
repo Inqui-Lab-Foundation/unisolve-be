b64494e57427c0d286f0cd75ad137f90719dc060
Inqui-LabFoundation <tech@inqui-lab.org>
2022-07-25 07:01:29 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -4,6 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
 export PM2_HOME=/home/ubuntu/.pm2
+pm2 save --force
 pm2 stop all
 pm2 delete all
 cd /home/ubuntu/unisolve-be
