2ef1c23dd2ec2bd3bf31da27f63318dce54a231f
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-09-09 00:39:57 +0530

    Added FQA count to getData

--- src/controllers/faq_category.controller.ts
+++ src/controllers/faq_category.controller.ts
@@ -4,9 +4,10 @@ import { constents } from "../configs/constents.config";
 import { speeches } from "../configs/speeches.config";
 import { faq } from "../models/faq.model";
 import dispatcher from "../utils/dispatch.util";
-import { faqCategorySchema,faqCategorySchemaUpdateSchema } from "../validations/faq_category.validations";
+import { faqCategorySchema, faqCategorySchemaUpdateSchema } from "../validations/faq_category.validations";
 import ValidationsHolder from "../validations/validationHolder";
 import BaseController from "./base.controller";
+import db from "../utils/dbconnection.util"
 
 export default class FaqCategoryController extends BaseController {
 
@@ -16,22 +17,22 @@ export default class FaqCategoryController extends BaseController {
         this.path = '/faqCategories';
     }
     protected initializeValidations(): void {
-        this.validations =  new ValidationsHolder(faqCategorySchema,faqCategorySchemaUpdateSchema);
+        this.validations = new ValidationsHolder(faqCategorySchema, faqCategorySchemaUpdateSchema);
     }
     protected initializeRoutes(): void {
         //example route to add 
         //this.router.get(`${this.path}/`, this.getData);
         super.initializeRoutes();
-        
-        
+
+
     }
 
     protected async getData(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try {
-            
+
             let data: any;
-            const { model, id} = req.params;
-            const paramStatus:any = req.query.status;
+            const { model, id } = req.params;
+            const paramStatus: any = req.query.status;
             if (model) {
                 this.model = model;
             };
@@ -39,56 +40,77 @@ export default class FaqCategoryController extends BaseController {
             const { page, size, title } = req.query;
             let condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
             const { limit, offset } = this.getPagination(page, size);
-            const modelClass = await this.loadModel(model).catch(error=>{
+            const modelClass = await this.loadModel(model).catch(error => {
                 next(error)
             });
             const where: any = {};
-            let whereClauseStatusPart:any = {};
-            if(paramStatus && (paramStatus in constents.common_status_flags.list)){
-                whereClauseStatusPart = {"status":paramStatus}
+            let whereClauseStatusPart: any = {};
+            // if(paramStatus && (paramStatus in constents.common_status_flags.list)){
+            //     whereClauseStatusPart = {"status":paramStatus}
+            // }
+            let whereClauseStatusPartLiteral = "1=1";
+            let addWhereClauseStatusPart = false
+            if (paramStatus && (paramStatus in constents.common_status_flags.list)) {
+                whereClauseStatusPart = { "status": paramStatus }
+                whereClauseStatusPartLiteral = `status = "${paramStatus}"`
+                addWhereClauseStatusPart = true;
             }
             if (id) {
                 where[`${this.model}_id`] = req.params.id;
-                data = await this.crudService.findOne(modelClass, { 
+                data = await this.crudService.findOne(modelClass, {
                     where: {
                         [Op.and]: [
                             whereClauseStatusPart,
                             where,
                         ]
                     },
-                    include:{
-                        model:faq,
-                        required:false  
-                    } 
+                    include: {
+                        model: faq,
+                        required: false
+                    }
                 });
             } else {
-                try{
-                    const responseOfFindAndCountAll = await this.crudService.findAndCountAll(modelClass, { 
+                try {
+                    const responseOfFindAndCountAll = await this.crudService.findAndCountAll(modelClass, {
+                        attributes: [
+                            'category_name',
+                            'faq_category_id',
+                            'status',
+                            'created_at',
+                            'created_by',
+                            'updated_at',
+                            'updated_by',
+                            [
+                                db.literal(`( SELECT COUNT(*) FROM faqs AS s WHERE
+                                ${addWhereClauseStatusPart ? "s." + whereClauseStatusPartLiteral : whereClauseStatusPartLiteral}
+                                AND s.faq_category_id = \`faq_category\`.\`faq_category_id\`)`), 'faq_count'
+                            ]
+                        ],
                         where: {
                             [Op.and]: [
                                 whereClauseStatusPart,
                                 condition
                             ]
                         },
-                        include:{
-                            model:faq,
-                            required:false  
-                        },
+                        // include: {
+                        //     model: faq,
+                        //     required: false
+                        // },
                         limit,
-                        offset 
+                        offset
                     })
                     const result = this.getPagingData(responseOfFindAndCountAll, page, limit);
                     data = result;
-                } catch(error:any){
+                } catch (error: any) {
                     return res.status(500).send(dispatcher(data, 'error'))
                 }
-                
+
             }
             // if (!data) {
             //     return res.status(404).send(dispatcher(data, 'error'));
             // }
             if (!data || data instanceof Error) {
-                res.status(200).send(dispatcher(null,"error",speeches.DATA_NOT_FOUND));
+                res.status(200).send(dispatcher(null, "error", speeches.DATA_NOT_FOUND));
                 // if(data!=null){
                 //     throw notFound(data.message)
                 // }else{
