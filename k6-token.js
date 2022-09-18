b064eed40b029a56e4c4faf1e6fcca21ec372a96
gururajhm <gurrajhm1977@gmail.com>
2022-09-18 19:26:57 +0530

    perf test1

--- k6-token.js
+++ k6-token.js
@@ -11,7 +11,7 @@ export const options = {
 ],
   thresholds: {
   //  http_req_failed: ["rate<0.10"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<250"], // 95% of requests should be below 350ms
+   // http_req_duration: ["p(95)<250"], // 95% of requests should be below 350ms
   },
   // httpDebug: "true",
   //userAgent: "K6GreetingsDemo/1.0",
