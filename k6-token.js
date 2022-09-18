0b30a502c1980d267bcb2f46222921c49ffc17c3
gururajhm <gurrajhm1977@gmail.com>
2022-09-18 19:23:24 +0530

    perf test1

--- k6-token.js
+++ k6-token.js
@@ -5,12 +5,12 @@ import { describe } from 'https://jslib.k6.io/expect/0.0.4/index.js';
  
 export const options = {  
   stages: [
-    { target: 2, duration: '10s' },
+    { target: 10, duration: '10s' },
     // { target: 20, duration: '1m' },
     // { target: 30, duration: '1m' },
 ],
   thresholds: {
-    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+  //  http_req_failed: ["rate<0.10"], // http errors should be less than 1%
     http_req_duration: ["p(95)<350"], // 95% of requests should be below 350ms
   },
   // httpDebug: "true",
