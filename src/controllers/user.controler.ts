bc3d864ce0b835bdcc0baa8be4723616f50b4c84
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-09-28 15:03:44 +0530

    Enabling user crud

---
+++ src/controllers/user.controler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { speeches } from '../configs/speeches.config';
+import dispatcher from '../utils/dispatch.util';
+import authService from '../services/auth.service';
+import BaseController from './base.controller';
+import ValidationsHolder from '../validations/validationHolder';
+
+export default class UserController extends BaseController {
+    model = "user";
+
+    protected initializePath(): void {
+        this.path = '/users';
+    }
+    protected initializeValidations(): void {
+        this.validations = new ValidationsHolder(null, null);
+    }
+    protected initializeRoutes(): void {
+        //example route to add
+        //this.router.get(`${this.path}/`, this.getData);
+        super.initializeRoutes();
+    }
+};
