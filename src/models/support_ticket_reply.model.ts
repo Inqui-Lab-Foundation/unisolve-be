5cbce2b64b563171b18ee08155224372d7b8d861
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-09-30 17:46:51 +0530

    added updated_by created_by

--- src/models/support_ticket_reply.model.ts
+++ src/models/support_ticket_reply.model.ts
@@ -1,6 +1,7 @@
 import { DataTypes, Model } from 'sequelize';
 import { constents } from '../configs/constents.config';
 import db from '../utils/dbconnection.util';
+import { student } from './student.model';
 import { support_ticket } from './support_ticket.model';
 
 export interface supportTicketRepliesAttributes {
@@ -67,4 +68,6 @@ support_ticket_reply.init(
 );
 
 support_ticket_reply.belongsTo(support_ticket, { foreignKey: 'support_ticket_id' });
+support_ticket_reply.belongsTo(student, { foreignKey: 'created_by'})
+support_ticket_reply.belongsTo(student, { foreignKey: 'updated_by'})
 support_ticket.hasMany(support_ticket_reply, { foreignKey: 'support_ticket_id' });
