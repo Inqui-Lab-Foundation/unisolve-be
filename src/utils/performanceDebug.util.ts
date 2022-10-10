d3e950a12bc308f839a6335ea205b86bedcd6b7b
amn <satija.aman@gmail.com>
2022-10-10 18:01:20 +0530

    added proper logs and fixed calculation of millis

--- src/utils/performanceDebug.util.ts
+++ src/utils/performanceDebug.util.ts
@@ -1,18 +1,24 @@
 export default class PerformanceDebug {
-    startTime:any = new Date();
-    endTime:any = new Date();
-    constructor(shouldStartAtInit=true){
+    startTime:any = new Date().getTime();
+    lastStepTime = new Date().getTime();
+    endTime:any = new Date().getTime();
+    constructor(logMsgPrefix="",shouldStartAtInit=true){
         if(shouldStartAtInit){
-            this.start()
+            this.start(logMsgPrefix)
         }
     }
-    start(){
+    start(logMsgPrefix=""){
         this.startTime = new Date();
+        console.log(`:::::::::::::: Prformance Debug started for ${logMsgPrefix } ::::::::::::::::`)
     }
 
     end(logMsgPrefix="",logMsgSuffix="",){
         this.endTime = new Date();
-        console.log(":"+logMsgPrefix+": time taken : "+ (this.endTime-this.startTime/1000) +"secs :"+logMsgSuffix+" : ")
+        console.log(`::::::::::::::  ${logMsgPrefix } ::::::::::::::::`)
+        console.log(":"+logMsgPrefix+": time taken from previous step : "+ (this.endTime-this.lastStepTime) +" millis :"+logMsgSuffix+" : ")
+        console.log(":"+logMsgPrefix+": total time taken : "+ (this.endTime-this.startTime) +" millis :"+logMsgSuffix+" : ")
+        console.log(`::::::::::::::  ${logMsgPrefix } ENDED ::::::::::::::::`)
+        this.lastStepTime = this.endTime;
     }
 
 }
