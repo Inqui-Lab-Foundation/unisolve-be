ccb31136f29ad8cec8f7454388b19dd44c40ba95
aman satija <satija.aman@gmail.com>
2022-10-12 15:40:38 +0530

    Dashboard apis : teacher stats : interium commit:started work

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -10,6 +10,8 @@ import { Op } from 'sequelize';
 import DashboardMapStatsJob from '../jobs/dashboardMapStats.jobs';
 import { dashboard_map_stat } from '../models/dashboard_map_stat.model';
 import DashboardService from '../services/dashboard.service';
+import { mentor } from '../models/mentor.model';
+import { organization } from '../models/organization.model';
 
 export default class DashboardController extends BaseController {
     model = ""; ///this u will override in every function in this controller ...!!!
@@ -23,13 +25,71 @@ export default class DashboardController extends BaseController {
     protected initializeRoutes(): void {
         //example route to add
         //this.router.get(`${this.path}/`, this.getData);
+
+        ///map stats
         this.router.get(`${this.path}/refreshMapStatsLive`, this.getMapStatsLive.bind(this))
         this.router.get(`${this.path}/mapStats`, this.getMapStats.bind(this))
         this.router.get(`${this.path}/refreshMapStats`, this.refreshMapStats.bind(this))
         
+
+        //mentor stats...
+        this.router.get(`${this.path}/mentorStats/:mentor_id`, this.getMentorStats.bind(this))
+        this.router.get(`${this.path}/mentorStats/:mentor_id/teamsCount`, this.getMentorStatsTeamCount.bind(this))
+        this.router.get(`${this.path}/mentorStats/:mentor_id/studentCount`, this.getMentorStatsStudentCount.bind(this))
+        this.router.get(`${this.path}/mentorStats/:mentor_id/ideasCount`, this.getMentorStatsIdeaCount.bind(this))
+        this.router.get(`${this.path}/mentorStats/:mentor_id/progessOverall`, this.getMentorStatsProgressOverall.bind(this))
+
         super.initializeRoutes();
     }
+    private async getMentorStats(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+            const {mentor_id} = req.params;
+            mentor.findOne({
+                where:{
+                    mentor_id:mentor_id,
+                },
+                attributes:[
+                    [
+                        db.literal('select count(user_id) from student where team_id in (select team_id from teams where)'),"no_of_students"
+                    ]
+                ],
+                include:{
+                    model:organization
+                }
+            })
 
+        }catch(err){
+            next(err)
+        }
+    }
+    private async getMentorStatsTeamCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+
+        }catch(err){
+            next(err)
+        }
+    }
+    private async getMentorStatsStudentCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+
+        }catch(err){
+            next(err)
+        }
+    }
+    private async getMentorStatsIdeaCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+
+        }catch(err){
+            next(err)
+        }
+    }
+    private async getMentorStatsProgressOverall(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+
+        }catch(err){
+            next(err)
+        }
+    }
     private async refreshMapStats(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try{
             const job = new DashboardMapStatsJob()
