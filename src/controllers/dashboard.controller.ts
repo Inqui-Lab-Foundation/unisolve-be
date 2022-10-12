c8cdc71ef86e17eec1ce84528d039d001908177b
amn <satija.aman@gmail.com>
2022-10-12 18:02:51 +0530

    dashboard apis : mentorStats api : added mentor stats api

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -12,6 +12,7 @@ import { dashboard_map_stat } from '../models/dashboard_map_stat.model';
 import DashboardService from '../services/dashboard.service';
 import { mentor } from '../models/mentor.model';
 import { organization } from '../models/organization.model';
+import { constents } from '../configs/constents.config';
 
 export default class DashboardController extends BaseController {
     model = ""; ///this u will override in every function in this controller ...!!!
@@ -34,9 +35,6 @@ export default class DashboardController extends BaseController {
 
         //mentor stats...
         this.router.get(`${this.path}/mentorStats/:mentor_id`, this.getMentorStats.bind(this))
-        this.router.get(`${this.path}/mentorStats/:mentor_id/teamsCount`, this.getMentorStatsTeamCount.bind(this))
-        this.router.get(`${this.path}/mentorStats/:mentor_id/studentCount`, this.getMentorStatsStudentCount.bind(this))
-        this.router.get(`${this.path}/mentorStats/:mentor_id/ideasCount`, this.getMentorStatsIdeaCount.bind(this))
         this.router.get(`${this.path}/mentorStats/:mentor_id/progessOverall`, this.getMentorStatsProgressOverall.bind(this))
 
         super.initializeRoutes();
@@ -44,45 +42,83 @@ export default class DashboardController extends BaseController {
     private async getMentorStats(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try{
             const {mentor_id} = req.params;
-            mentor.findOne({
+            const paramStatus:any= req.query.status;
+            let whereClauseStatusPart:any = {};
+            let whereClauseStatusPartLiteral = "1=1";
+            let addWhereClauseStatusPart = false
+            if(paramStatus && (paramStatus in constents.common_status_flags.list)){
+                whereClauseStatusPart = {"status":paramStatus}
+                whereClauseStatusPartLiteral = `status = "${paramStatus}"`
+                addWhereClauseStatusPart =true;
+            }
+
+            const mentor_stats = await mentor.findOne({
                 where:{
                     mentor_id:mentor_id,
                 },
                 attributes:[
                     [
-                        db.literal('select count(user_id) from student where team_id in (select team_id from teams where)'),"no_of_students"
+                        db.literal(`(
+                        select count(s.student_id) 
+                        from students as s
+                        where 
+                        ${addWhereClauseStatusPart?"s."+whereClauseStatusPartLiteral:whereClauseStatusPartLiteral}
+                        and s.team_id in (
+                            select team_id 
+                            from teams as t
+                            where 
+                            ${addWhereClauseStatusPart?"t."+whereClauseStatusPartLiteral:whereClauseStatusPartLiteral}
+                            and t.mentor_id=\`mentor\`.\`mentor_id\`)
+                            )`),
+                        "students_count"
+                    ],
+                    [
+                        db.literal(`(
+                        select count(c.team_id) 
+                        from challenge_responses as c 
+                        where c.team_id in (
+                            select team_id 
+                            from teams as t
+                            where 
+                            ${addWhereClauseStatusPart?"t."+whereClauseStatusPartLiteral:whereClauseStatusPartLiteral}
+                            and t.mentor_id=\`mentor\`.\`mentor_id\`) 
+                        and c.status not in ('DRAFT')
+                        )`),
+                        "ideas_count"
+                    ],
+                    [
+                        db.literal(`(
+                        select count(t.team_id) 
+                        from teams as t
+                        where 
+                        ${addWhereClauseStatusPart?"t."+whereClauseStatusPartLiteral:whereClauseStatusPartLiteral}
+                        and t.mentor_id=\`mentor\`.\`mentor_id\`
+                        )`),
+                        "teams_count"
                     ]
                 ],
                 include:{
-                    model:organization
+                    model:organization,
+                    attributes:[
+                        'organization_name',
+                        'district'
+                    ]
+
                 }
             })
-
+            if(mentor_stats instanceof Error){
+                throw mentor_stats
+            }
+            if(mentor_stats){
+                res.status(200).json(dispatcher(res,mentor_stats,"success"))
+            }else{
+                res.status(500).json(dispatcher(res,"somethign went wrong","error"))
+            }
         }catch(err){
             next(err)
         }
     }
-    private async getMentorStatsTeamCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        try{
-
-        }catch(err){
-            next(err)
-        }
-    }
-    private async getMentorStatsStudentCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        try{
-
-        }catch(err){
-            next(err)
-        }
-    }
-    private async getMentorStatsIdeaCount(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        try{
-
-        }catch(err){
-            next(err)
-        }
-    }
+    
     private async getMentorStatsProgressOverall(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try{
 
