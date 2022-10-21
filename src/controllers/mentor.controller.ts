17fd1a53c4855f10402681a765f18edd06dcddba
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-10-21 09:35:13 +0530

    hashed OTP

--- src/controllers/mentor.controller.ts
+++ src/controllers/mentor.controller.ts
@@ -67,10 +67,11 @@ export default class MentorController extends BaseController {
         }
         // const otp = await this.authService.generateOtp();
         let otp = await this.authService.triggerOtpMsg(req.body.mobile); //async function but no need to await ...since we yet do not care about the outcome of the sms trigger ....!!this may need to change later on ...!!
-        // let hashString = await this.authService.HashPassword(otp)
         otp = String(otp)
+        let hashString = await this.authService.HashPassword(otp);
+        console.log(hashString);
         const updatePassword = await this.authService.crudService.update(user,
-            { password: await bcrypt.hashSync(otp, process.env.SALT || baseConfig.SALT) },
+            { password: await bcrypt.hashSync(hashString, process.env.SALT || baseConfig.SALT) },
             { where: { user_id: result.dataValues.user_id } });
         const data = result.dataValues;
         return res.status(201).send(dispatcher(res, data, 'success', speeches.USER_REGISTERED_SUCCESSFULLY, 201));
